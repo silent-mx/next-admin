2f22526a0cd3aab4fb28bc2b91f58797d3002408
silent-mx <silentmx@outlook.com>
2024-05-27 12:08:46 +0800

    feat(auth):添加refresh_token

--- auth.ts
+++ auth.ts
@@ -22,7 +22,6 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
         username: { label: "用户名或邮箱", type: "text" },
         password: { label: "密码", type: "password" },
       },
-
       async authorize(credentials) {
         const tokenRes = await fetch(`${process.env.NEXT_API_HOST}/connect/token`, {
           method: "POST",
@@ -72,52 +71,93 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
         if (!appConfigRes.ok) {
           throw new Error("app初始化失败");
         }
-        const appConfig = (await appConfigRes.json()) ?? {};
+        const appConfig = (await appConfigRes.json()) ?? null;
         if (appConfig.currentUser && appConfig.currentUser.isAuthenticated) {
           (account as any).access_token = (user as any).access_token;
           (account as any).refresh_token = (user as any)?.refresh_token;
-          (account as any).expires_in = Date.now() + ((user as any)?.expires_in - 600) * 1000;
           (account as any).token_type = (user as any)?.token_type;
+          (account as any).expires_in = (user as any)?.expires_in * 1000;
+          (account as any).expires_at = Date.now() + (user as any)?.expires_in * 1000;
+
           (account as any).phone_number = appConfig.currentUser.phoneNumber;
           (account as any).auth = Object.keys(appConfig.auth.grantedPolicies);
-          account.userId = appConfig.currentUser.id;
 
           (user as any).access_token = undefined;
           (user as any).refresh_token = undefined;
           (user as any).expires_in = undefined;
           (user as any).token_type = undefined;
+          user.id = appConfig.currentUser.id;
           user.name = appConfig.currentUser.userName;
           user.email = appConfig.currentUser.email;
 
           return true;
         } else {
-          return false;
+          throw new Error("登录失败");
         }
       } else {
-        return false;
+        throw new Error("登录失败");
       }
     },
     async jwt({ token, account }) {
-      console.log(token);
       if (account) {
-        token.access_token = account.access_token;
-        token.refresh_token = account.refresh_token;
-        token.expires_in = account.expires_in;
-        token.token_type = account.token_type;
-        token.phone_number = account.phone_number;
-        token.auth = account.auth;
-        token.userId = account.userId;
-      }
+        return {
+          access_token: account.access_token,
+          refresh_token: account.refresh_token,
+          expires_at: account.expires_at,
+          token_type: account.token_type,
+          auth: account.auth,
+          user: {
+            ...token,
+            phone_number: account.phone_number,
+          },
+        };
+      } else if (Date.now() < (token as any).expires_at) {
+        return token;
+      } else {
+        if (!token.refresh_token) {
+          throw new Error("Missing refresh_token");
+        }
 
-      return token;
+        try {
+          const refreshTokenRes = await fetch(`${process.env.NEXT_API_HOST}/connect/token`, {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/x-www-form-urlencoded",
+            },
+            body: new URLSearchParams({
+              grant_type: "refresh_token",
+              refresh_token: (token as any).refresh_token,
+              client_id: "OrderSys_Web",
+              client_secret: "",
+            }),
+          });
+
+          const refreshTokenData = await refreshTokenRes.json();
+
+          if (!refreshTokenRes.ok) {
+            throw refreshTokenData;
+          }
+
+          return {
+            ...token,
+            access_token: refreshTokenData.access_token,
+            refresh_token: refreshTokenData.refresh_token ?? token.refresh_token,
+            expires_at: Date.now() + refreshTokenData.expires_in * 1000,
+          };
+        } catch (error) {
+          console.error("Error refreshing access token", error);
+          return { ...token, error: "RefreshAccessTokenError" as const };
+        }
+      }
     },
     async session({ session, token }) {
-      (session as any).user.id = token.userId;
+      (session as any).error = token.error;
       (session as any).user.phone_number = token.phone_number;
-      (session as any).access_token = token.access_token;
-      (session as any).expires = token.expires_in;
-      (session as any).auth = token.auth;
-      return session;
+      (session as any).expires = token.expires_at;
+      return {
+        ...session,
+        ...token,
+      };
     },
   },
 });
--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,19 @@
 import { auth } from "@/auth";
 
 export default auth((req) => {
-  // console.log(req.auth);
+  if (!req.auth) {
+    if (!req.nextUrl.pathname.startsWith("/login")) {
+      const redirectUrl = new URL("/login", req.nextUrl as unknown as URL);
+      redirectUrl.searchParams.set("redirect", req.nextUrl.pathname);
+      return Response.redirect(redirectUrl);
+    }
+  } else {
+    if (req.nextUrl.pathname.startsWith("/login")) {
+      return Response.redirect(new URL("/", req.nextUrl as unknown as URL));
+    }
+  }
 });
 
 export const config = {
-  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
+  matcher: ["/((?!api|_next/static|_next/image|favicon.ico|image/.*).*)"],
 };
